05f04d95d04edd1f9fcaa71ff456bb6af0a3866b
eswar-87 <eswariot48@gmail.com>
2026-06-09 14:46:53 +0530

    Add Procfile and update ALLOWED_HOSTS for deployment

--- weatherapp/weatherapp/settings.py
+++ weatherapp/weatherapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-9353xm!^akg@z7_ps2xql!+i)204qcz(u6%_3j^4_5jylx$*f^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
